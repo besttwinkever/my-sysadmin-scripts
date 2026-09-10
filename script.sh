4de465c3f973cf24c19d2e787f06c641ea916105
German <mrcreepton@github.com>
2026-09-10 10:00:16 +0000

    Первая версия

---
+++ script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+DELAY=10
+LOG_FILE="monitor.log"
+
+while true; do
+    echo "--- $(date '+%Y-%m-%d %H:%M:%S') ---" >> "$LOG_FILE"
+    free -h >> "$LOG_FILE"
+    df -h >> "$LOG_FILE"
+    uptime >> "$LOG_FILE"
+
+    sleep "$DELAY"
+done
